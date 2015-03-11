f7e143f0f11ad9ab3eb32a7ac964437ee3515e4b
Alex H <hehe3301@gmail.com>
2015-03-11 14:05:46 -0400

    Adjusting list

--- src/node/Tree.java
+++ src/node/Tree.java
@@ -1,17 +1,18 @@
 package node;
 
-import java.awt.List;
+import java.util.ArrayList;
 import java.util.Dictionary;
 
 public class Tree {
 	Node head = null;
 	Dictionary<String, Double> dict;
-	List vars = new List();
-	
+	ArrayList<String> vars = new ArrayList<String>();
+	int numVars = 0;
 	
 	public Boolean addNode(Node node){
-		if(node.getType()==2){
+		if(node.getType()==2 && !vars.contains(node.getValue()) ){ //if what are are adding is a variable and we don't already know about it
 			vars.add(node.getValue());
+			numVars++;
 		}
 		if(head.placeNode(node)){
 			return true;
